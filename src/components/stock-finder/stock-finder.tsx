a0b40cf677f2cb8d931289ec0833cc2bca964789
Leticia Lopes <leticialopes236@gmail.com>
2023-05-01 20:38:23 -0300

    outputting a list of elements

--- src/components/stock-finder/stock-finder.tsx
+++ src/components/stock-finder/stock-finder.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from "@stencil/core"
+import { Component, h, State } from "@stencil/core"
 import { AV_API_KEY } from '../../global/global'
 
 @Component ({
@@ -10,13 +10,17 @@ import { AV_API_KEY } from '../../global/global'
 export class StockFinder {
   stockNameInput: HTMLInputElement;
 
+  @State() searchResults: {symbol: string, name: string}[] = []; // os resultados de pesquisa é uma lista de objetos onde cada objeto tem um símbolo o qual é uma string e um nome o qual é uma string também
+
   onFindStocks(event: Event) {
     event.preventDefault();
     const stockName = this.stockNameInput.value;
     fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
     .then(res => res.json())
     .then(parsedRes => {
-      console.log(parsedRes)
+      this.searchResults = parsedRes['bestMatches'].map(match => { // os resultados estão dentro do objeto 'bestMatches', então sempre dê um console.log para se certificar
+        return {name: match['2. name'], symbol: match['1. symbol']}
+      })
     }).catch(err => {
       console.log(err)
     })
@@ -30,7 +34,12 @@ export class StockFinder {
       ref={el => this.stockNameInput = el}
       />
       <button type="submit">Find</button>
-    </form>];
+    </form>,
+    <ul>
+      {this.searchResults.map(result => <li>{result.name}</li>)} 
+    </ul>
+    // map percorre o array e retorna um outro array com a mesma quantidade de elementos
+  ];
 
   }
 }
